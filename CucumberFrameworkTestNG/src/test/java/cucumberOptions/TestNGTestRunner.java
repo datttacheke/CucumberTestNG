46f214af25a8a30238a58c0a48b860d710f7a1d1
dattacheke@gmail.com <dattacheke@gmail.com>
2024-02-02 02:12:40 +0530

    tags change

--- CucumberFrameworkTestNG/src/test/java/cucumberOptions/TestNGTestRunner.java
+++ CucumberFrameworkTestNG/src/test/java/cucumberOptions/TestNGTestRunner.java
@@ -6,7 +6,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(features="src/test/java/features",glue ="stepDefinitions"
-,monochrome=true, tags ="@PlaceOrder", //tags ="@PlaceOrder or @OffersPage",
+,monochrome=true, //tags ="@PlaceOrder", //tags ="@PlaceOrder or @OffersPage",
 plugin= {"html:target/cucumber.html", "json:target/cucumber.json",
 "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
 "rerun:target/failed_scenarios.txt"})
