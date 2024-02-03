81c4234057358adc68c0befbfae300d992e3fe29
dattacheke@gmail.com <dattacheke@gmail.com>
2024-02-04 02:13:15 +0530

    jkskh

--- CucumberFrameworkTestNG/src/main/java/rahulshettyacademy/CucumberFramework/App.java
+++ CucumberFrameworkTestNG/src/main/java/rahulshettyacademy/CucumberFramework/App.java
@@ -4,14 +4,14 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class App {
-	
-	public static void main(String[] args) {
+  public static void main(String[] args) {
     System.out.println("Hello World!");
-    System.setProperty("webdriver.firefox.bin", "C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\Firefox.lnk");
-    System.setProperty("webdriver.gecko.driver", 
-    		"E:\\TESTING\\Softwars\\geckodriver.exe");
-    WebDriver driver = new FirefoxDriver();
-    System.out.println("louch FirefoxDriver");
-    
+	/*
+	 * System.setProperty("webdriver.gecko.driver",
+	 * "E:\\TESTING\\Softwars\\geckodriver.exe");
+	 */
+
+	WebDriver driver = new FirefoxDriver();
+	 System.out.println("Louch FirefoxDriver");
   }
 }
--- CucumberFrameworkTestNG/src/test/java/utils/TestBase.java
+++ CucumberFrameworkTestNG/src/test/java/utils/TestBase.java
@@ -15,15 +15,19 @@ public class TestBase {
 	public WebDriver driver;
 
 	public WebDriver WebDriverManager() throws IOException {
-		FileInputStream fis = new FileInputStream(
+		FileInputStream fis1 = new FileInputStream(
 				"E:\\TESTING\\CucumberSeleniumProject\\CucumberFrameworkJunit\\src\\test\\resources\\global.properties");
+		FileInputStream fis = new FileInputStream(
+				System.getProperty("user.dir") + "//src//test//resources//global.properties");
+
 		Properties prop = new Properties();
 		prop.load(fis);
 		String url = prop.getProperty("QAUrl");
 		String browser_properties = prop.getProperty("browser");
 		String browser_maven = System.getProperty("browser");
 		// result = testCondition ? value1 : value2
-		System.out.println(browser_maven);
+		System.out.println("browser_properties: " + browser_properties);
+		System.out.println("browser_maven" + browser_maven);
 		String browser = browser_maven != null ? browser_maven : browser_properties;
 
 		if (driver == null) {
@@ -37,9 +41,10 @@ public class TestBase {
 				 */
 			}
 			if (browser.equalsIgnoreCase("firefox")) {
-				System.setProperty("webdriver.gecko.driver", 
-						"E:\\TESTING\\Softwars\\geckodriver.exe");
+				// System.setProperty("webdriver.gecko.driver",
+				// "E:\\TESTING\\Softwars\\geckodriver.exe");
 				driver = new FirefoxDriver();
+				System.out.println("Firefox browser is open");
 			}
 			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 			driver.get(url);
