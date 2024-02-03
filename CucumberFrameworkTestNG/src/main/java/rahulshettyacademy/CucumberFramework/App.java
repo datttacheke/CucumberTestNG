86a472dc913d9aa8a0a6547c740e75bde89ddac4
dattacheke@gmail.com <dattacheke@gmail.com>
2024-02-04 01:20:53 +0530

    changes

--- CucumberFrameworkTestNG/src/main/java/rahulshettyacademy/CucumberFramework/App.java
+++ CucumberFrameworkTestNG/src/main/java/rahulshettyacademy/CucumberFramework/App.java
@@ -1,8 +1,17 @@
 package rahulshettyacademy.CucumberFramework;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class App {
-  public static void main(String[] args) {
+	
+	public static void main(String[] args) {
     System.out.println("Hello World!");
-    System.out.println("Hello World!");
+    System.setProperty("webdriver.firefox.bin", "C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\Firefox.lnk");
+    System.setProperty("webdriver.gecko.driver", 
+    		"E:\\TESTING\\Softwars\\geckodriver.exe");
+    WebDriver driver = new FirefoxDriver();
+    System.out.println("louch FirefoxDriver");
+    
   }
 }
