c6634a99554b1a2960665f050e0b940134966d03
dattacheke@gmail.com <dattacheke@gmail.com>
2024-02-05 02:02:50 +0530

    added by Architect

---
+++ CucumberFrameworkTestNG/src/main/java/rahulshettyacademy/CucumberFramework/DevClass.java
@@ -0,0 +1,10 @@
+package rahulshettyacademy.CucumberFramework;
+
+public class DevClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("added bt architect");
+	}
+
+}
