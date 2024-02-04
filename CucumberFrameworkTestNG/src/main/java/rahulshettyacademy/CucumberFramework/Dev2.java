76d8fb640603e8d340bc8a6a767dfb14267a44b9
dattacheke@gmail.com <dattacheke@gmail.com>
2024-02-05 02:38:34 +0530

    added ind arch

---
+++ CucumberFrameworkTestNG/src/main/java/rahulshettyacademy/CucumberFramework/Dev2.java
@@ -0,0 +1,11 @@
+package rahulshettyacademy.CucumberFramework;
+
+public class Dev2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Added by me");
+
+	}
+
+}
